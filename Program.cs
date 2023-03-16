5f75632887f3f851afebbc0632d99c5044b70dc1
shykeiichi <mail@keii.dev>
2023-03-16 10:59:49 +0100

    Working

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
 
 
 class Token
@@ -33,6 +34,13 @@ class TokenNumber : Token
     public float value;
 
     public TokenNumber(float value) {
+        this.value = value;
+    }
+}
+
+class TokenBoolean : Token {
+    public bool value;
+    public TokenBoolean(bool value) {
         this.value = value;
     }
 }
@@ -84,7 +92,8 @@ class Program {
         string fileContents = "";
         foreach(string line in fileContentsArray)
         {
-            string line2 = line + "\n";
+            string line2 = line.Split("//").First();
+            line2 = line2 + "\n";
             
             fileContents += line2.TrimStart().TrimEnd();
         }
@@ -106,19 +115,29 @@ class Program {
             newFileContents += c;
         }
 
-        fileContents = newFileContents;
+        fileContents = "{" +  newFileContents  + "}";
 
         List<Token> stack = new List<Token>();
         
         string currentWord = "";
         isString = false;
         
+        var boolvalues = new String[2]{"true", "false"};
+
         foreach (char c in fileContents)
         {
             switch (c)
             {
                 case '=':
-                    stack.Add(new TokenIdentifier(currentWord));
+                    if (boolvalues.Contains(currentWord))
+                    {
+                        stack.Add(new TokenBoolean(currentWord == "true"));    
+                    }
+                    else
+                    {
+                        stack.Add(new TokenIdentifier(currentWord));
+                    }
+
                     currentWord = "";
                     stack.Add(new TokenAssign());
                     break;
@@ -130,7 +149,15 @@ class Program {
                             stack.Add(new TokenNumber(value));
                             currentWord = "";
                         } else {
-                            stack.Add(new TokenIdentifier(currentWord));
+                            if (boolvalues.Contains(currentWord))
+                            {
+                                stack.Add(new TokenBoolean(currentWord == "true"));    
+                            }
+                            else
+                            {
+                                stack.Add(new TokenIdentifier(currentWord));
+                            }
+
                             currentWord = "";
                         }
                     }
@@ -143,8 +170,22 @@ class Program {
                 case ']':
                     if (currentWord != "")
                     {
-                        stack.Add(new TokenIdentifier(currentWord));
-                        currentWord = "";
+                        float value = 0f;
+                        if(float.TryParse(currentWord, out value)) {
+                            stack.Add(new TokenNumber(value));
+                            currentWord = "";
+                        } else {
+                            if (boolvalues.Contains(currentWord))
+                            {
+                                stack.Add(new TokenBoolean(currentWord == "true"));    
+                            }
+                            else
+                            {
+                                stack.Add(new TokenIdentifier(currentWord));
+                            }
+
+                            currentWord = "";
+                        }
                     }
 
                     stack.Add(new TokenArrayEnd());
@@ -155,8 +196,22 @@ class Program {
                 case '}':
                     if (currentWord != "")
                     {
-                        stack.Add(new TokenIdentifier(currentWord));
-                        currentWord = "";
+                        float value = 0f;
+                        if(float.TryParse(currentWord, out value)) {
+                            stack.Add(new TokenNumber(value));
+                            currentWord = "";
+                        } else {
+                            if (boolvalues.Contains(currentWord))
+                            {
+                                stack.Add(new TokenBoolean(currentWord == "true"));    
+                            }
+                            else
+                            {
+                                stack.Add(new TokenIdentifier(currentWord));
+                            }
+
+                            currentWord = "";
+                        }
                     }
 
                     stack.Add(new TokenDictEnd());
@@ -167,7 +222,15 @@ class Program {
                 case ')':
                     if (currentWord != "")
                     {
-                        stack.Add(new TokenIdentifier(currentWord));
+                        if (boolvalues.Contains(currentWord))
+                        {
+                            stack.Add(new TokenBoolean(currentWord == "true"));    
+                        }
+                        else
+                        {
+                            stack.Add(new TokenIdentifier(currentWord));
+                        }
+
                         currentWord = "";
                     }
 
@@ -198,55 +261,166 @@ class Program {
         return stack.ToArray();
     }
 
-    static Dictionary<String, dynamic> ParseConfig(Token[] stack)
+    static dynamic ParseDict(Token[] tokens)
     {
-        Dictionary<String, dynamic> parsedConfig = new Dictionary<string, dynamic>();
-        List<dynamic> depth = new List<dynamic>();
-
-        int i = -1;
-        foreach (Token t in stack)
-        {
-            i++;
-
-            Console.WriteLine(t);
-
-            if(t.GetType() == typeof(TokenArrayStart))
-            {
-                depth.Add("arr");
-            }
-
-            if(t.GetType() == typeof(TokenArrayEnd))
-            {
-                depth.RemoveAt(depth.Count - 1);
-            }
-
-            foreach (var d in depth)
-            {
-                Console.Write(d + ", ");
-            }
-            Console.WriteLine();
+        Dictionary<string, dynamic> json_object = new Dictionary<string, dynamic>();
+        var t = tokens[0];
+        if(t.GetType() == typeof(TokenDictEnd)) {
+            return new dynamic[] {json_object, tokens.ToList().Skip(1).ToArray()};
         }
 
-        return parsedConfig;
+        while(true) {
+            var json_key = tokens[0];
+            if(json_key.GetType() == typeof(TokenIdentifier)) {
+                tokens = tokens.ToList().Skip(1).ToArray();
+            } else {
+                Console.WriteLine("Expected key got: " + json_key);
+                Environment.Exit(3);
+            }
+
+            if(tokens[0].GetType() != typeof(TokenAssign)) {
+                Console.WriteLine("Excpected assing after key in dict got: " + t);
+                Environment.Exit(4);
+            }
+
+            var tmp = Parse(tokens.ToList().Skip(1).ToArray());
+            var json_value = tmp[0];
+            tokens = tmp[1];
+
+            if(json_value.GetType() == typeof(TokenIdentifier)) {
+                Console.WriteLine("Excpected string | number | bool got identifier: " + ((TokenIdentifier)json_value).value);
+                Environment.Exit(4);
+            }
+
+            json_object.Add((string)(((TokenIdentifier)json_key).value), json_value);
+
+            t = tokens[0];
+            if(t.GetType() == typeof(TokenDictEnd)) {
+                return new dynamic[] {json_object, tokens.ToList().Skip(1).ToArray()};
+            } else if(t.GetType() != typeof(TokenComma)) {
+                Console.WriteLine("Expected assign after pair in object got: " + t);
+                Environment.Exit(5);
+            }
+
+            tokens = tokens.ToList().Skip(1).ToArray();
+        }
+    }
+
+    static dynamic ParseArray(Token[] tokens)
+    {
+        List<dynamic> json_array = new List<dynamic>();
+
+        var t = tokens[0];
+        if(t.GetType() == typeof(TokenArrayEnd)) {
+            return new dynamic[] {json_array, tokens.ToList().Skip(1).ToArray()};
+        }
+
+        while(true) {
+            var tmp = Parse(tokens);
+            var json = tmp[0];
+            tokens = tmp[1];
+
+            if(json.GetType() == typeof(TokenString) || json.GetType() == typeof(TokenNumber) || json.GetType() == typeof(TokenBoolean) || json.GetType() == typeof(TokenIdentifier)) {
+                json_array.Add(((dynamic)json).value);
+            } else {
+                json_array.Add(json);
+            }
+
+            t = tokens[0];
+            if(t.GetType() == typeof(TokenArrayEnd)) {
+                return new dynamic[] {json_array, tokens.ToList().Skip(1).ToArray()};
+            } else if(t.GetType() != typeof(TokenComma)) {
+                Console.WriteLine("Expected comma after object in array");
+                Environment.Exit(1);
+            } else {
+                tokens = tokens.ToList().Skip(1).ToArray();
+            }
+        }
+    }
+
+    static dynamic Parse(Token[] tokens) {
+        var t = tokens[0];
+
+        // Console.WriteLine("Parsing Array");
+        if (t.GetType() == typeof(TokenArrayStart))
+        {
+            // Console.WriteLine("Parsing Array: " + t);
+            return ParseArray(tokens.ToList().Skip(1).ToArray());
+        } else if (t.GetType() == typeof(TokenDictStart))
+        {
+            // Console.WriteLine("Parsing Dict: " + t);
+            return ParseDict(tokens.ToList().Skip(1).ToArray());
+        }
+        else
+        {
+            return new dynamic[] {t, tokens.ToList().Skip(1).ToArray()};
+        }
+    }
+
+    static string SerializeObject(dynamic tokens) {
+        string output = "";
+        foreach(dynamic i in tokens) {
+            if(i.GetType() == typeof(Dictionary<string, dynamic>)) {
+                output += ("{ ");
+                foreach(KeyValuePair<string, dynamic> j in (Dictionary<string, dynamic>)i) {
+                    if(j.Value.GetType() == typeof(TokenIdentifier) || j.Value.GetType() == typeof(TokenString) || j.Value.GetType() == typeof(TokenNumber) || j.Value.GetType() == typeof(TokenBoolean)) { 
+                        if(j.Value.GetType() == typeof(TokenString)) {
+                            output += (j.Key + ": \"" + (dynamic)j.Value.value + "\", ");
+                        } else {
+                            output += (j.Key + ": " + j.Value.value + ", ");
+                        }
+                    } else {
+                        if(j.Value.GetType() == typeof(List<dynamic>)) {
+                            output += (j.Key + ": [" + SerializeObject(j.Value) + "], ");
+                        } else {
+                            output += (j.Key + ": {" + SerializeObject(j.Value) + "}, ");
+                        }
+                    }
+                    // output += "\n";
+                }
+                output += (" } ");
+            } else if(i.GetType() == typeof(List<dynamic>)) {
+                foreach(dynamic j in i) {
+                    if(j.GetType() == typeof(List<dynamic>)) {
+                        if(j.GetType() == typeof(List<dynamic>)) {
+                            output += "[" + SerializeObject(j) + "], "; 
+                        } else {
+                            output += "{" + SerializeObject(j) + "}, ";
+                        }                    
+                        // output += ", ";
+                    } else {
+                        if(j.GetType() == typeof(TokenString)) {
+                            output += "\"" + j + "\", ";
+                        } else {
+                            output += j + ", ";
+                        }
+                    }
+                }
+            }
+        }
+        return output;
     }
 
     static void Main(string[] args)
     {
-        var stack = TokenizeFile("./examples/array.fcf");
+        var stack = TokenizeFile("./examples/fullconfig.fcf");
 
-        // foreach (dynamic c in stack)
-        // {
-        //     if (c.GetType() == typeof(TokenIdentifier) || c.GetType() == typeof(TokenString) ||
-        //         c.GetType() == typeof(TokenNumber))
-        //     {
-        //         Console.WriteLine(c + " " + c.value);
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine(c);
-        //     }
-        // }
-        //
-        var parsedConfig = ParseConfig(stack);
+        // var d = Parse(stack);
+        // // Console.WriteLine();        
+        // Console.WriteLine(SerializeObject(d));
+        // // Console.WriteLine();
+        // // foreach(var i in d[0]) {
+        // //     Console.WriteLine(i);
+        // // }
+        // Console.WriteLine(d[0]["arr"][1]);
+
+        foreach(var d in stack) {
+            if(d.GetType() == typeof(TokenString) || d.GetType() == typeof(TokenNumber) || d.GetType() == typeof(TokenBoolean) || d.GetType() == typeof(TokenIdentifier)) {
+                Console.WriteLine(d + ": " + ((dynamic)d).value);
+            } else {
+                Console.WriteLine(d);
+            }
+        }
+        
     }
 }
